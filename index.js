972b2691e7cc2121617606e195d555d066754fab
Peter Soboyejo <thepcmrtim@gmail.com>
2017-05-24 23:05:17 -0400

    ghjk

--- index.js
+++ index.js
@@ -191,18 +191,6 @@ function startMenu() {
 var base_url;
 var userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'
 
-/*
-Nightmare.action('show', function(name, options, parent, win, renderer, done) {
-    parent.respondTo('show', function(done) {
-        win.show();
-        done();
-    });
-    done();
-}, function(done) {
-    this.child.call('show', done);
-});
-*/
-
 prompt.message = 'Input';
 var match,
     styleID,
