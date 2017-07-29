ccc0d83404ac4fdb3541562ef5b3fcaad4155f79
Luqmaan <ldawoodjee@gmail.com>
2017-07-28 20:03:56 -0500

    Refactor 2

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    node: true,
+    es6: true,
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  plugins: ['prettier'],
+  parserOptions: {
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': ['off'],
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'all',
+        bracketSpacing: true,
+        jsxBracketSameLine: true,
+      },
+    ],
+  },
+};
